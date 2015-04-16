5cc4fd18eb8170efc6b16c119957ae920ba08a3b
ross <rossklin@gmail.com>
2015-04-16 07:02:27 +0200

    wrote grid routines (not compile ready)

--- grid_tree.cpp
+++ grid_tree.cpp
@@ -3,37 +3,101 @@
 using namespace std;
 using namespace st3;
 
-grid_tree::node::node(grid_tree::value_type v){
-  parent = 0;
-  lower_child = 0;
-  upper_child = 0;
-  count = 0;
-  value = v;
+grid::grid(){
+  max_leaves = 10;
+  root = new node(&index, 0);
 }
 
-grid_tree::node::~node(){
-  if (lower_child) delete lower_child;
-  if (upper_child) delete upper_child;
+grid::~grid(){
+  delete root;
 }
 
-void grid_tree::node::insert_value(grid_tree::value_type v){
-  if (count){
-    if (!(upper_child && lower_child)){
-      cout << "missing child" << endl;
-      exit(-1);
-    }
-    
-    float x = align ? v.second.y : v.second.x;
-    if (x < split){
-      lower_child -> insert_value(v);
-    }else{
-      upper_child -> insert_value(v);
-    }
-  }else{
-    // todo: find best split
-    // todo: make children
-    // todo: place v in one child, value in the other
+grid::node::node(grid::grid *i, grid::node *p){
+  g = i;
+  parent = p;
+  is_leaf = true;
+  bounds = bound_type(point(-Infinity, -Infinity), point(Infinity, Infinity));
+}
+
+grid::node::~node(){
+  if (!is_leaf){
+    for (int i = 0; i < 4; i++) if (children[i]) delete children[i];
+  }
+}
+
+void grid::node::make_split(){
+  // compute mid point
+  split.x = split.y = 0;
+
+  for (auto x : leaves) split = split + x.second;
+  split = utility::scale_point(split, 1 / (float)leaves.size());
+
+  // make child nodes
+  for (int i = 0; i < 4; i++) children[i] = new node(g, this);
+  children[0] -> bounds = {bounds.first, split};
+  children[1] -> bounds = {point(split.x, bounds.first.y), point(bounds.second.x, split.y)};
+  children[2] -> bounds = {point(bounds.first.x, split.y), point(split.x, bounds.second.y)};
+  children[3] -> bounds = {split, bounds.second};
+
+  // insert child values
+  for (auto x : leaves){
+    point v = x.second;
+    int idx = (v.x > split.x) + 2 * (v.y > split.y);
+    children[idx] -> insert(x.first, x.second);
   }
 
-  count++;
+  leaves.clear();
+  is_leaf = false;
+}
+
+void grid::node::insert(grid::key_type k, grid::value_type v){
+  if (is_leaf){
+    leaves[k] = v;
+    g -> index[k] = this;
+
+    if (leaves.size() > g -> max_leaves){
+      make_split();
+    }
+  }else{
+    int idx = (v.x > split.x) + 2 * (v.y > split.y);
+    children[idx] -> insert(k, v);
+  }
+}
+
+void grid::node::move(grid::key_type k, grid::value_type v){
+  if (!leaves.count(k)){
+    cout << "node: missing leaf: " << k << endl;
+    exit(-1);
+  }
+
+  if (utility::point_between(v, bounds.first, bounds.second)){
+    leaves[k] = v;
+  }else{
+    leaves.erase(k);
+    g -> root -> insert(k,v);
+  }
+}
+
+list<grid::iterator_type> grid::node::search(grid::value_type p, float r){
+  list<grid::iterator_type> res;
+
+  if (is_leaf){
+    // collect leaves
+    float r2 = r * r;
+    for (auto x : leaves){
+      point delta = x.second - p;
+      float d2 = delta.x * delta.x + delta.y * delta.y;
+      if (d2 <= r2){
+	res.push_back(x);
+      }
+    }
+  }else{
+    list<grid::iterator_type> q;
+    for (int i = 0; i < 4; i++){
+      q = children[i] -> search(p, r);
+      res.insert(res.end(), q.begin(), q.end());
+    }
+  }
+
+  return res;
 }
--- grid_tree.h
+++ grid_tree.h
@@ -1,25 +1,47 @@
 #ifndef _STK_GRIDTREE
 #define _STK_GRIDTREE
 
+#include <SFML/Graphics.hpp>
 #include "types.h"
 
 namespace st3{
-  namespace grid_tree{
-    typedef std::pair<idtype, point> value_type;
-    struct node{
-      node *parent;
-      node *lower_child;
-      node *upper_child;
-      bool align; // false = x, true = y
-      float split; 
-      int count; // number of children
-      value_type value; // node has either children or value
+  struct grid{
+    typedef idtype key_type;
+    typedef point value_type;
+    typedef std::pair<point, point> bound_type;
+    typedef hm_t<key_type, node*> index_type;
 
-      node(value_type v);
+    struct node{
+      grid *g;
+      node *parent;
+      bool is_leaf;
+      bound_type bounds;
+
+      // branch node
+      point split;
+      node *children[4]; // UL, UR, BL, BR
+
+      // leaf node
+      hm_t<key_type, value_type> leaves;
+
+      node(grid *g, node *p);
+      node(const node &n) = delete;
       ~node();
-      void insert_value(value_type v) = 0;
-      std::list<value_type> search(point p, float r) = 0;
+      void make_split();
+      bound_type insert(key_type k, value_type v); // return leaf node
+      std::list<value_type> search(point p, float r);
     };
+
+    index_type index;
+    int max_leaves;
+    node *root;
+
+    grid();
+    ~grid();
+    grid(const grid &g) = delete;
+    void insert(key_type id, value_type p);
+    void move(key_type id, value_type p);
+    std::list<value_type> search(value_type p, float r);
   };
 };
 
