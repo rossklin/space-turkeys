204da3f2ebc4bae3d885ff255752b867ed5f4863
Ross Linscott <rossklin@gmail.com>
2017-05-05 20:50:02 +0200

    Added forgotten file.

---
+++ src/data_frame.h
@@ -0,0 +1,18 @@
+#ifndef _STK_DATA_FRAME
+#define _STK_DATA_FRAME
+
+#include "selector.h"
+#include "game_settings.h"
+#include "player.h"
+#include "types.h"
+
+namespace st3{
+  struct data_frame{
+    game_settings settings; /*!< the game settings */
+    hm_t<idtype, player> players; /*!< data for players in the game */
+    hm_t<combid, client::entity_selector::ptr> entity; /*!< graphical representations for solars, fleets and waypoints */
+    std::list<combid> remove_entities;
+  };
+};
+
+#endif
