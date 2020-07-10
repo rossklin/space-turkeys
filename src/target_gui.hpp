b06902edae1c88a6a8fcc93ce902b714374d7796
Ross Linscott <rossklin@gmail.com>
2020-07-10 17:12:41 +0200

    Updated target_gui

--- src/target_gui.hpp
+++ src/target_gui.hpp
@@ -1,56 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include <set>
+#pragma once
+
+#include <functional>
+#include <list>
 #include <string>
 
-#include "graphics.hpp"
-#include "types.hpp"
+#include "rsg/src/RskTypes.hpp"
 
 namespace st3 {
-
-/*! gui for allocating ships to command selectors */
-class target_gui {
- public:
-  struct option_t {
-    combid key;
-    std::string option;
-
-    option_t(combid k, std::string v);
-  };
-
-  static const option_t option_cancel;
-  static const option_t option_add_waypoint;
-
- private:
-  std::vector<option_t> options;
-  int highlight_index;
-  window_t *window;
-  sf::FloatRect bounds;
-
-  static const point option_size;
-
-  int compute_index(point p);
-
- public:
-  point position;
-  option_t selected_option;
-  std::list<combid> selected_entities;
-  bool done;
-
-  /*! construct a target gui
-      @param p coordinate position
-      @param options list of target options to show
-      @param sel list of selected entities to set up commands for
-      @param w pointer to window for coorinate transform and drawing
-    */
-  target_gui(point p, std::list<option_t> options, std::list<combid> sel, window_t *w);
-
-  /*! handle an event
-      @param e the event
-      @return whether the event was effective 
-    */
-  bool handle_event(sf::Event e);
-
-  /*! draw the gui */
-  void draw();
-};
+RSG::PanelPtr target_gui(sf::Vector2f position, std::list<std::string> options, std::function<void(std::string)> callback);
 };  // namespace st3
