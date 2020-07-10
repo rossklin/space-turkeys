4146b339a7f0bcbbaca3057933002c90ab643a0b
Ross Linscott <rossklin@gmail.com>
2020-07-10 17:12:52 +0200

    Updated command_gui

--- src/command_gui.cpp
+++ src/command_gui.cpp
@@ -1,157 +1,87 @@
 #include "command_gui.hpp"
 
-#include <cstdlib>
-#include <iostream>
-#include <set>
+#include <functional>
+#include <list>
 #include <string>
-#include <unordered_map>
-#include <utility>
 
-#include "client_game.hpp"
-#include "desktop.hpp"
 #include "fleet.hpp"
-#include "graphics.hpp"
-#include "selector.hpp"
-#include "solar.hpp"
+#include "rsg/src/RskTypes.hpp"
+#include "rsg/src/button.hpp"
+#include "rsg/src/panel.hpp"
+#include "rsg/src/slider.hpp"
+#include "rsg/src/utility.hpp"
+#include "style.hpp"
 #include "utility.hpp"
 
 using namespace std;
 using namespace st3;
-using namespace sfg;
-using namespace interface;
+using namespace RSG;
 
-const std::string command_gui::sfg_id = "command gui";
+PanelPtr command_gui(
+    string ship_class,
+    string action,
+    int policy,
+    int num_available,
+    bool allow_combat,
+    Voidfun on_cancel,
+    function<void(int, int)> on_commit) {
+  // Policy panel
+  hm_t<int, string> policies = {
+      {fleet::policy_maintain_course, "Maintain course"},
+      {fleet::policy_aggressive, "Aggressive"},
+      {fleet::policy_evasive, "Evasive"},
+  };
+  list<ComponentPtr> policy_buttons;
 
-command_gui::Ptr command_gui::Create(client::command_selector::ptr c, client::game *g) {
-  Ptr self(new command_gui(c, g));
-  self->Add(self->layout);
-  self->SetAllocation(main_interface::qw_allocation);
-  self->SetId(sfg_id);
-  return self;
-}
-
-command_gui::command_gui(client::command_selector::ptr c, client::game *g) : Window(Window::Style::BACKGROUND) {
-  layout = Box::Create(Box::Orientation::VERTICAL, 10);
-  // add header and policies
-  string header = c->source + ": " + c->action + " " + c->target;
-  layout->Pack(Label::Create(header));
-  layout->Pack(Separator::Create());
-
-  hm_t<int, RadioButton::Ptr> policy;
-  policy[fleet::policy_aggressive] = RadioButton::Create("Aggressive");
-  policy[fleet::policy_evasive] = RadioButton::Create("Evasive", policy[fleet::policy_aggressive]->GetGroup());
-  policy[fleet::policy_maintain_course] = RadioButton::Create("Maintain Course", policy[fleet::policy_aggressive]->GetGroup());
-
-  selected_policy = c->policy;
-  policy[selected_policy]->SetActive(true);
-
-  for (auto p : policy) {
-    int fp = p.first;
-    p.second->GetSignal(ToggleButton::OnToggle).Connect([this, fp]() {
-      selected_policy = fp;
-    });
-    layout->Pack(p.second);
-  }
-
-  layout->Pack(Separator::Create());
-
-  for (auto sid : g->get_ready_ships(c->source)) {
-    ship_ptr s = g->get_specific<ship>(sid);
-    data[s->ship_class].available++;
-  }
-
-  for (auto sid : c->ships) {
-    ship_ptr s = g->get_specific<ship>(sid);
-    data[s->ship_class].allocated++;
-  }
-
-  point label_dims(80, 40);
-
-  auto build_ship_label = [this](string ship_class) -> string {
-    return to_string(data[ship_class].allocated) + "/" + to_string(data[ship_class].allocated + data[ship_class].available);
+  auto policy_setter = [&policy, &policy_buttons](int x) {
+    return [&policy, &policy_buttons, x](ButtonPtr s) {
+      policy = x;
+      for (auto b : policy_buttons) b->remove_state("selected");
+      s->add_state("selected");
+    };
   };
 
-  // build ship allocation tables for each available ships class
-  Box::Ptr ships_layout = Box::Create(Box::Orientation::VERTICAL, 5);
-  for (string ship_class : ship::all_classes()) {
-    int total = data[ship_class].available + data[ship_class].allocated;
-    if (total == 0) continue;
-
-    Box::Ptr box = Box::Create(Box::Orientation::HORIZONTAL, 10);
-    Scale::Ptr scale = Scale::Create(sfg::Scale::Orientation::HORIZONTAL);
-    scale->SetRequisition(sf::Vector2f(400, 10));
-    data[ship_class].alloc_label = Label::Create(build_ship_label(ship_class));
-    data[ship_class].adjust = scale->GetAdjustment();
-    data[ship_class].adjust->SetLower(0);
-    data[ship_class].adjust->SetUpper(total);
-    data[ship_class].adjust->SetValue(data[ship_class].allocated);
-    data[ship_class].adjust->GetSignal(Adjustment::OnChange).Connect([this, g, build_ship_label, ship_class, total]() {
-      int value = min((int)data[ship_class].adjust->GetValue(), g->get_max_ships_per_fleet(g->self_id));
-      data[ship_class].allocated = value;
-      data[ship_class].available = total - data[ship_class].allocated;
-      data[ship_class].alloc_label->SetText(build_ship_label(ship_class));
-
-      // set all other ship classes to 0
-      for (auto sc2 : ship::all_classes()) {
-        if (sc2 == ship_class) continue;
-        // data[sc2].available += data[sc2].allocated;
-        // data[sc2].allocated = 0;
-        data[sc2].adjust->SetValue(0);
-      }
-    });
-
-    box->Pack(Label::Create(ship_class));
-    box->Pack(scale);
-    box->Pack(data[ship_class].alloc_label);
-    ships_layout->Pack(box);
+  for (auto x : policies) {
+    ButtonPtr b = Button::create(x.second, policy_setter(x.first));
+    if (x.first == policy) b->add_state("selected");
+    policy_buttons.push_back(b);
   }
 
-  layout->Pack(graphics::wrap_in_scroll(ships_layout, false, 300));
+  PanelPtr policy_panel = styled<Panel, list<ComponentPtr>>(
+      {},
+      {
+          make_label("Policy:"),
+          Panel::create(policy_buttons),
+      });
 
-  // add ok and cancel
-  Button::Ptr ok = Button::Create("OK");
-  auto on_ok = [this, c, g]() {
-    c->policy = selected_policy;
+  // Slider
+  int num = num_available;
+  ButtonPtr slider_label = make_label("Ships to send: " + to_string(num));
 
-    // assign correct number of ships per class to c
-    c->ships.clear();
-    set<combid> ready_ships = g->get_ready_ships(c->source);
-
-    // first list ships by class
-    hm_t<string, set<combid> > by_class;
-    for (auto sid : ready_ships) {
-      ship_ptr s = g->get_specific<ship>(sid);
-      by_class[s->ship_class].insert(s->id);
-    }
-
-    // then assign ships
-    for (auto x : data) {
-      string ship_class = x.first;
-      int count = x.second.allocated;
-      for (int i = 0; i < count; i++) {
-        auto iter = by_class[ship_class].begin();
-        combid sid = *iter;
-        by_class[ship_class].erase(iter);
-        c->ships.insert(sid);
-      }
-    }
-
-    desktop->clear_qw();
+  Slider::handler_t slider_handler = [&num, slider_label](SliderPtr self, float new_value, float new_ratio) {
+    num = new_value;
+    slider_label->set_label("Ships to send: " + to_string(num));
   };
-  desktop->bind_ppc(ok, on_ok);
 
-  Button::Ptr cancel = Button::Create("Cancel");
-  desktop->bind_ppc(cancel, []() { desktop->clear_qw(); });
+  SliderPtr slider = Slider::create(0, num_available, num_available, slider_handler);
 
-  Button::Ptr b_delete = Button::Create("Delete");
-  desktop->bind_ppc(b_delete, [g, c]() {
-    desktop->clear_qw();
-    g->remove_command(c->id);
-  });
+  PanelPtr slider_panel = Panel::create({slider_label, slider}, Panel::ORIENT_VERTICAL);
 
-  Box::Ptr l_bottom = Box::Create(Box::Orientation::HORIZONTAL, 10);
-  l_bottom->Pack(ok);
-  l_bottom->Pack(cancel);
-  l_bottom->Pack(b_delete);
-  layout->Pack(l_bottom);
+  // Button panel
+  ButtonPtr b_cancel = Button::create("Cancel", [on_cancel](ButtonPtr b) { on_cancel(); });
+  ButtonPtr b_commit = Button::create("Commit", [on_commit, &policy, &num](ButtonPtr b) { on_commit(policy, num); });
+  PanelPtr button_panel = Panel::create({b_cancel, b_commit});
+
+  return styled<Panel, list<ComponentPtr>, Panel::orientation>(
+      {},
+      {
+          make_label("Assign ships for '" + action + "' command"),
+          make_hbar(),
+          policy_panel,
+          make_hbar(),
+          slider_panel,
+          make_hbar(),
+          button_panel,
+      },
+      Panel::ORIENT_VERTICAL);
 }
--- src/command_gui.hpp
+++ src/command_gui.hpp
@@ -1,38 +1,17 @@
-#include <SFGUI/Widgets.hpp>
-#include <SFML/Graphics.hpp>
-#include <set>
+#pragma once
+
+#include <functional>
 #include <string>
 
-#include "selector.hpp"
-#include "types.hpp"
+#include "rsg/src/RskTypes.hpp"
 
 namespace st3 {
-namespace client {
-class game;
-};
-
-namespace interface {
-/*! gui for allocating ships to command selectors */
-class command_gui : public sfg::Window {
-  struct class_info {
-    int available;
-    int allocated;
-    sfg::Adjustment::Ptr adjust;
-    sfg::Label::Ptr alloc_label;
-  };
-
-  hm_t<std::string, class_info> data;
-  int selected_policy;
-
- public:
-  typedef std::shared_ptr<command_gui> Ptr;
-  typedef std::shared_ptr<const command_gui> PtrConst;
-  static Ptr Create(client::command_selector::ptr c, client::game *g);
-  static const std::string sfg_id;
-
-  sfg::Box::Ptr layout;
-
-  command_gui(client::command_selector::ptr c, client::game *g);
-};
-};  // namespace interface
+RSG::PanelPtr command_gui(
+    std::string ship_class,
+    std::string action,
+    int policy,
+    int num_available,
+    bool allow_combat,
+    RSG::Voidfun on_cancel,
+    std::function<void(int, int)> on_commit);
 };  // namespace st3
