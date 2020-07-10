3c64a8d0247f4c6a608ce25f99fdca1a99e80fa3
Ross Linscott <rossklin@gmail.com>
2020-07-10 12:15:03 +0200

    Partial implementation of choice_gui

--- src/choice_gui.cpp
+++ src/choice_gui.cpp
@@ -4,21 +4,65 @@
 #include <list>
 #include <string>
 
-#include "RskTypes.hpp"
-#include "panel.hpp"
+#include "rsg/src/RskTypes.hpp"
+#include "rsg/src/button.hpp"
+#include "rsg/src/panel.hpp"
+#include "style.hpp"
+#include "utility.hpp"
 
 using namespace std;
 using namespace st3;
 using namespace RSG;
 
-RSG::PanelPtr choice_gui(
+PanelPtr build_queue(list<string> q) {
+  // todo
+}
+
+PanelPtr build_info(list<string> info) {
+  // todo
+}
+
+PanelPtr choice_gui(
     std::string title,
     std::list<string> options,
     option_generator f_opt,
+    info_generator f_info,
     std::function<void(std::list<std::string>)> on_commit,
     RSG::Voidfun on_cancel,
     bool allow_queue) {
-  return Panel::create({
+  // Queue wrapper
+  PanelPtr queue_wrapper = Panel::create();
+  list<string> queue;
+  auto enque = [&](string v) {
+    queue.push_back(v);
+    queue_wrapper->clear_children();
+    queue_wrapper->add_child(build_queue(queue));
+  };
 
-  });
+  // Info wrapper
+  PanelPtr info_wrapper = Panel::create();
+
+  list<ComponentPtr> cards = utility::map<list<string>, list<ComponentPtr>>(
+      [=](string v) -> ComponentPtr {
+        ButtonPtr b = f_opt(v);
+        b->set_on_press([=](ButtonPtr self) { enque(v); });
+        b->on_hover = [=]() {
+          info_wrapper->clear_children();
+          info_wrapper->add_child(build_info(f_info(v)));
+        };
+        return b;
+      },
+      options);
+
+  return Panel::create(
+      {
+          make_label(title),
+          make_hbar(),
+          Panel::create(cards),
+          make_hbar(),
+          info_wrapper,
+          make_hbar,
+          queue_wrapper,
+      },
+      Panel::ORIENT_VERTICAL);
 }
--- src/choice_gui.hpp
+++ src/choice_gui.hpp
@@ -8,11 +8,13 @@
 #include "types.hpp"
 
 namespace st3 {
-typedef std::function<RSG::ComponentPtr(std::string)> option_generator;
+typedef std::function<RSG::ButtonPtr(std::string)> option_generator;
+typedef std::function<std::list<std::string>(std::string)> info_generator;
 RSG::PanelPtr choice_gui(
     std::string title,
     std::list<string> options,
     option_generator f_opt,
+    info_generator f_info,
     std::function<void(std::list<std::string>)> on_commit,
     RSG::Voidfun on_cancel,
     bool allow_queue);
