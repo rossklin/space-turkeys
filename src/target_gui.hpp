33d33a49a70d704747da3609cccdffd786881015
Ross Linscott <rossklin@gmail.com>
2020-07-12 16:37:51 +0200

    Updated target_gui header to handle actions and targets

--- src/target_gui.hpp
+++ src/target_gui.hpp
@@ -2,10 +2,17 @@
 
 #include <functional>
 #include <list>
+#include <set>
 #include <string>
 
 #include "rsg/src/RskTypes.hpp"
+#include "types.hpp"
 
 namespace st3 {
-RSG::PanelPtr target_gui(sf::Vector2f position, std::list<std::string> options, std::function<void(std::string)> callback);
+RSG::PanelPtr target_gui(
+    sf::Vector2f position,
+    hm_t<std::string, std::set<combid>> action_targets,
+    // To create waypoint, set action to fleet::go_to and target to identifier::source_none
+    std::function<void(std::string action, std::string target)> callback,
+    RSG::Voidfun on_cancel);
 };  // namespace st3
