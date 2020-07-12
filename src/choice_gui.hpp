0f27917c08c1049bd8133c0389438e7b90ca3998
Ross Linscott <rossklin@gmail.com>
2020-07-12 16:36:47 +0200

    Added action parameter to choice_gui header

--- src/choice_gui.hpp
+++ src/choice_gui.hpp
@@ -8,6 +8,13 @@
 #include "types.hpp"
 
 namespace st3 {
+
+enum choice_gui_action {
+  CHOICEGUI_APPEND,
+  CHOICEGUI_PREPEND,
+  CHOICEGUI_REPLACE
+};
+
 typedef std::function<RSG::ButtonPtr(std::string)> option_generator;
 typedef std::function<std::list<std::string>(std::string)> info_generator;
 RSG::PanelPtr choice_gui(
@@ -15,7 +22,7 @@ RSG::PanelPtr choice_gui(
     std::list<string> options,
     option_generator f_opt,
     info_generator f_info,
-    std::function<void(std::list<std::string>)> on_commit,
+    std::function<void(choice_gui_action a, std::list<std::string> q)> on_commit,
     RSG::Voidfun on_cancel,
     bool allow_queue);
 };  // namespace st3
