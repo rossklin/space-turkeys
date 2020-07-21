d5d38a9619417d8362dfdf2984f3c169ece46f6e
Ross Linscott <rossklin@gmail.com>
2020-07-21 10:56:48 +0200

    Added style make_card Reduced make_hbar margins

--- src/style.cpp
+++ src/style.cpp
@@ -26,9 +26,26 @@ ButtonPtr st3::make_hbar() {
           {"height", "1px"},
           {"border-thickness", "0"},
           {"background-color", "11223388"},
-          {"margin-top", "20"},
-          {"margin-bottom", "20"},
+          {"margin-top", "10"},
+          {"margin-bottom", "10"},
           {"margin-left", "0"},
           {"margin-right", "0"},
       });
+}
+
+ButtonPtr st3::make_card(string v, int n, string h) {
+  int w = (100 - 3 * (n + 1)) / n;
+  return styled<Button>(
+      {
+          {"width", to_string(w) + "%"},
+          {"height", h},
+          {"font-size", "40"},
+          {"background-color", "123456ff"},
+          {"hover:background-color", "123456aa"},
+          {"selected:color", "ffffffff"},
+          {"selected:background-color", "654321aa"},
+          {"selected:border-thickness", "2"},
+          {"selected:border-color", "ffffff88"},
+      },
+      v);
 }
--- src/style.hpp
+++ src/style.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <functional>
+#include <string>
 
 #include "rsg/src/RskTypes.hpp"
 
 namespace st3 {
 RSG::ButtonPtr make_label(std::string v);
 RSG::ButtonPtr make_hbar();
+RSG::ButtonPtr make_card(std::string v, int n, std::string h = "200px");
 }  // namespace  st3
