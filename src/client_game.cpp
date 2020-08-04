ed987fff752112fbb9134d1ed26f4f1a2ec53e88
Ross Linscott <rossklin@gmail.com>
2020-08-04 18:11:04 +0200

    Client game: missed removal of ref to choice::fleets

--- src/client_game.cpp
+++ src/client_game.cpp
@@ -997,9 +997,6 @@ choice game::build_choice() {
     if (x->isa(waypoint::class_id)) {
       c.waypoints[x->id] = get_specific<waypoint>(x->id);
       cout << "build choice: " << x->id << endl;
-    } else if (x->isa(fleet::class_id)) {
-      c.fleets[x->id] = get_specific<fleet>(x->id);
-      cout << "build_choice: " << x->id << endl;
     } else if (x->isa(solar::class_id) && x->owned) {
       c.solar_choices[x->id] = get_specific<solar>(x->id)->choice_data;
       cout << "build_choice: " << x->id << endl;
