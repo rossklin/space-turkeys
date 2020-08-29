d810995228089a71b7cfff3e1a6c26dc5ef03ad7
Ross Linscott <rossklin@gmail.com>
2020-08-29 20:48:51 +0200

    Protocol: change to enum

--- src/protocol.hpp
+++ src/protocol.hpp
@@ -3,39 +3,44 @@
 #include "types.hpp"
 
 namespace st3 {
-/*! constants used in client server communication */
 namespace protocol {
+/*! constants used in client server communication */
+enum protocol : sint {
 
-/* **************************************** */
-/* MENU QUERIES */
-/* **************************************** */
+  /* **************************************** */
+  /* MENU QUERIES */
+  /* **************************************** */
 
-const sint get_status = 100;
-const sint create_game = 101;
-const sint join_game = 102;
-const sint disconnect = 103;
+  get_status,
+  create_game,
+  join_game,
+  disconnect,
 
-/* **************************************** */
-/* GAME QUERIES */
-/* **************************************** */
+  /* **************************************** */
+  /* GAME QUERIES */
+  /* **************************************** */
 
-const sint game_round = 0; /*!< query whether to start the game round */
-const sint choice = 1;     /*!< query whether to send the choice */
-const sint frame = 2;      /*!< query to get a simulation frame */
-const sint connect = 3;    /*!< query to connect */
-const sint leave = 4;
-const sint load_init = 5;
-const sint id = 6;
-const sint any = 7;
+  game_round,
+  choice,
+  frame,
+  connect,
+  leave,
+  load_init,
+  id,
+  any,
 
-/* **************************************** */
-/* RESPONSES */
-/* **************************************** */
+  /* **************************************** */
+  /* RESPONSES */
+  /* **************************************** */
 
-const sint confirm = 1000;  /*!< confirm a query */
-const sint complete = 1001; /*!< tell the client the game is complete */
-const sint invalid = 1002;  /*!< tell the client the query is invalid */
-const sint aborted = 1003;  /*!< tell the client the game was aborted */
-const sint standby = 1004;
+  confirm,
+  complete,
+  invalid,
+  aborted,
+  standby,
+
+  // limit
+  NUM
+};
 };  // namespace protocol
 };  // namespace st3
